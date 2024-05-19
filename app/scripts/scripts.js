116c5f2d90eb95e9247831fc35a4673914640e02
JamesChuong <jcjameschuong@gmail.com>
2024-05-18 17:32:05 -0700

    Add CSS to homepage, removed html form sending POST request

---
+++ app/scripts/scripts.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.getElementById('imageUploader');
+    const fileInput = document.getElementById('select');
+    const fileNameSpan = document.getElementById('file-name');
+});
